f3244d7b5423376d0eb15d5836f5fd80ac8f86bd
Moebyte <47306349+Moebyte@users.noreply.github.com>
2026-06-18 12:11:54 +0800

    Load Frenchtown entry module

--- src/story-modules.js
+++ src/story-modules.js
@@ -11,6 +11,8 @@
     'src/story-chapters/chapter-2-home-xuehua-contract.js',
     'src/story-chapters/chapter-2-home-fixed.js',
     'src/story-chapters/chapter-2-home-fixed-contract.js',
+    'src/story-chapters/chapter-2-frenchtown-entry.js',
+    'src/story-chapters/chapter-2-frenchtown-entry-contract.js',
     'src/story-chapters/chapter-2-frenchtown-tail.js',
     'src/story-chapters/chapter-2-frenchtown-tail-contract.js',
     'src/story-chapters/chapter-2-xuehua-203.js',
