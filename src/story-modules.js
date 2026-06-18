887771f67f1b21a07e37c94fea69eb8b3a17f2ab
Moebyte <47306349+Moebyte@users.noreply.github.com>
2026-06-18 12:21:38 +0800

    Load leave home module

--- src/story-modules.js
+++ src/story-modules.js
@@ -9,6 +9,8 @@
     'src/story-chapters/chapter-2-home-fixed-contract.js',
     'src/story-chapters/chapter-2-home-talk.js',
     'src/story-chapters/chapter-2-home-talk-contract.js',
+    'src/story-chapters/chapter-2-leave-home.js',
+    'src/story-chapters/chapter-2-leave-home-contract.js',
     'src/story-chapters/chapter-2-frenchtown-entry.js',
     'src/story-chapters/chapter-2-frenchtown-entry-contract.js',
     'src/story-chapters/chapter-2-frenchtown-tail.js',
